763e83c39c26895ca2c90e0aa48ecfc213e4ba5b
yongseog-yun <yongseog.yun@navercorp.com>
2025-02-08 00:11:06 +0900

    configure cognito - client secret

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_C5wpUDMyV', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3bfk0t3cif2p7bhi20ciiif5v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '1fs09n2dobtk9m0sm04buh6dor9d75mmq78khd9rtr7jgetn0joe',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
